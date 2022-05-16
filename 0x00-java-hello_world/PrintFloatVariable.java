5d26290b885181d35e173ef05c471fbfe91c2ad6
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 16:28:29 -0300

    FLoat

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
         float numero = (float) 3.142;
-        System.out.println ( "Valor de pi:" + String.format("%.3f\n", numero));
+        System.out.println ( "Valor de pi: " + String.format("%.3f", numero) + "\n");
     }
 };
