b35c4f1bef95b2debe1c48fa9c90c40e78ba710a
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 16:58:40 -0300

    concat

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main(String[] args) {
         String texto1 = new String ("Holberton");
         String texto2 = new String (" School");
-        String texto3 = texto1 + texto2;
-        System.out.print ("Bem vindo a " + texto3 + "!\r");
+        String texto3 = new String ("Bem vindo a " + texto1 + texto2 + "!";
+        System.out.print (texto3 + "\r");
     }
 };
