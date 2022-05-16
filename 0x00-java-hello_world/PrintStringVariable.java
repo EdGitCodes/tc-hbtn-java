940115c5729e1e0c469b6fb170b2a17c9e9c0058
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 16:41:08 -0300

    substring

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = new String ("Holberton School");
-        System.out.print (texto + "\n" + texto + "\n" + texto.substring(10));
+        System.out.println (texto + "\n" + texto + "\n" + texto.substring(0,9));
     }
 };
