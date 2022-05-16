949088f6517337c673fc1d1766363b3e4597ca36
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 15:52:48 -0300

    FLoat

---
+++ 0x00-java-hello_world/PrintFloatIntegerVariable.java
@@ -0,0 +1,6 @@
+public class PrintFloatVariable {
+    public static void main(String[] args) {
+        float numero = (float) 3.1421;
+        System.out.print ( String.format("%.3f", numero));
+    }
+}
---
+++ src/PrintFloatVariable.java
@@ -0,0 +1,8 @@
+import java.math.*;
+
+public class PrintFloatVariable {
+    public static void main(String[] args) {
+        float numero = (float) 3.1421;
+        System.out.print ( String.format("%.3f", numero));
+    }
+}
