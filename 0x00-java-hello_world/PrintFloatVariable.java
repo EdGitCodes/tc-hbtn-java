8ce0eca7594aaccbe56d5064578cd88400632012
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 16:03:11 -0300

    Check chato da ALura

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
-        float numero = (float) 3,142;
+        float numero = (float) 3.142;
         System.out.print ( String.format("%.3f", numero));
     }
-}
+};
