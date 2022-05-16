9f680df7e591557c36db34a83f96da80079ab2e2
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-16 16:54:22 -0300

    Concat

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
         float numero = (float) 3.142;
-        System.out.print( "Valor de pi: " + String.format("%.3f", numero)+ "\n");
+        System.out.print( "Valor de pi: " + String.format("%.3f", numero)+ "\r");
     }
 };
---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String texto1 = new String ("Holberton");
+        String texto2 = new String (" School");
+        String texto3 = texto1 + texto2;
+        System.out.print ("Bem vindo a " + texto3 + "\r");
+    }
+};
