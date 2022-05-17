c666de370972eeb5cac8c034712ef6e291064219
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-17 10:45:34 -0300

    teste

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -7,19 +7,11 @@ public class PrintFormatacaoNumeros {
 
         float taxa = 0.2456f;
         float valor = 7654.321f;
+        Locale localeBR = new Locale("en","US");
 
-        Locale localeBR = new Locale("pt","BR");
+        NumberFormat dinheiro = NumberFormat.getInstance(localeBR);
+        System.out.println("Valor: R$ " + dinheiro.format(valor));
+        System.out.printf("Taxa: %.02f%%", taxa);
 
-        NumberFormat percentual = NumberFormat.getPercentInstance(localeBR);
-        NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
-
-        String taxafinal = new String("0." + percentual.format(taxa));
-        String valors = new String (dinheiro.format(valor));
-        String valor1 = valors.substring(0,6).replace(".",",");
-        String valor2 = valors.substring(6,11).replace(",",".");
-        String valorfinal = valor1+valor2;
-
-        System.out.println("Valor: " + valorfinal);
-        System.out.println("Taxa: "  + taxafinal);
     }
 }
