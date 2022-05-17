5c004e31e8ae5075c61ad5cf12a7107694b75583
Luis Eduardo dos Santos <CIANDT\luises@lnb036019cps.ciandt.global>
2022-05-17 09:32:53 -0300

    num format

--- src/TesteStrings.java
+++ src/TesteStrings.java
@@ -1,5 +1,6 @@
 import java.text.NumberFormat;
 import java.util.*;
+import java.math.*;
 
 
 public class TesteStrings {
@@ -8,14 +9,14 @@ public class TesteStrings {
         float taxa = 0.2456f;
         float valor = 7654.321f;
         Locale localeBR = new Locale("pt","BR");
-
         NumberFormat percentual = NumberFormat.getPercentInstance(localeBR);
         NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
+        String taxafinal = new String("0." + percentual.format(taxa));
         String valors = new String (dinheiro.format(valor));
         String valor1 = valors.substring(0,6).replace(".",",");
         String valor2 = valors.substring(6,11).replace(",",".");
         String valorfinal = valor1+valor2;
         System.out.println("Valor: " + valorfinal);
-        System.out.println("Taxa: 0." + percentual.format(taxa));
+        System.out.println("Taxa: "  + taxafinal);
     }
 }
