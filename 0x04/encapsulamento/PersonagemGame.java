f439803e551be44b94f77dbe2e90d81ccef5a266
Luis <luis_8.51@hotmail.com>
2022-05-18 12:33:37 -0300

    Metodos

---
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -0,0 +1,21 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+    public String getNome (){
+        return nome;
+    }
+
+    public int getSaudeAtual(){
+        return saudeAtual;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setSaudeAtual (int saudeAtual){
+        this.saudeAtual = saudeAtual;
+    }
+}
---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,46 @@
+public class PersonagemGame {
+
+    private int quantidadeDeDano;
+    private int quantidadeDeCura;
+    private int saudeAtual;
+    private String nome;
+
+    public int tomarDano(int quantidadeDeDano) {
+        if(saudeAtual > 0){
+            this.saudeAtual = this.saudeAtual - quantidadeDeDano;
+            if (saudeAtual < 0) {
+                this.saudeAtual = 0;
+            }
+        }else{
+            this.saudeAtual = 0;
+        }
+        return saudeAtual;
+    }
+
+    public int receberCura(int quantidadeDeCura) {
+        if(saudeAtual < 100){
+            this.saudeAtual = quantidadeDeCura + this.saudeAtual;
+            if (saudeAtual > 100) {
+                this.saudeAtual = 100;
+            }
+        }
+        return saudeAtual;
+    }
+
+
+    public String getNome (){
+        return nome;
+    }
+
+    public int getSaudeAtual(){
+        return saudeAtual;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setSaudeAtual (int saudeAtual){
+        this.saudeAtual = saudeAtual;
+    }
+}
