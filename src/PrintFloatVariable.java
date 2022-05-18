19290f11985efa57e73ca16e932c71ec38a9f0d8
EdGitCodes <77654778+EdGitCodes@users.noreply.github.com>
2022-05-18 12:04:35 -0300

    Pasta errada

--- src/PrintFloatVariable.java
+++
@@ -1,8 +0,0 @@
-import java.math.*;
-
-public class PrintFloatVariable {
-    public static void main(String[] args) {
-        float numero = (float) 3.1421;
-        System.out.print ( String.format("%.3f\n", numero ));
-    }
-}
