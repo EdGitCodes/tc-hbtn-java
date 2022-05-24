69e64157af57cee1cf1972fd0e7388c0a7b90f7f
Luis <luis_8.51@hotmail.com>
2022-05-24 11:57:35 -0300

    Quadrado

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,26 @@
+//package quadrado;
+
+public class Quadrado extends Retangulo{
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado < 0){
+            throw new java.lang.IllegalArgumentException("Lado deve ser maior igual a 0");
+        }else {
+            this.lado = lado;
+        }
+    }
+
+    public double area(){
+        return (lado*lado);
+    }
+    public String toString(){
+        return ("[Quadrado] " + lado);
+    }
+
+}
---
+++ 0x05/quadrado/Retangulo.java
@@ -0,0 +1,39 @@
+//package quadrado;
+
+public class Retangulo extends FormaGeometrica{
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if(largura < 0){
+            throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }else {
+            this.largura = largura;
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if(altura < 0){
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }else {
+            this.altura = altura;
+        }
+    }
+    @Override
+    public double area(){
+        return (largura*altura);
+    }
+
+    public String toString(){
+
+        return ("[Retangulo] " + String.format("%.2f",largura) + " / " + String.format("%.2f",altura));
+    }
+}
