fdddbeff4fb09bb30b0907aee2cf6444849781df
Luis <luis_8.51@hotmail.com>
2022-05-24 12:08:30 -0300

    Abstrato

---
+++ 0x05/abstrata/Quadrado.java
@@ -0,0 +1,25 @@
+public class Quadrado extends Retangulo {
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado < 0){
+            throw new IllegalArgumentException("Lado deve ser maior igual a 0");
+        }else {
+            this.lado = lado;
+        }
+    }
+
+    @Override
+    public double area(){
+        return (lado*lado);
+    }
+    public String toString(){
+        return ("[Quadrado] " + String.format("%.2f",lado));
+    }
+
+}
