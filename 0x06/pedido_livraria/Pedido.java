f0ca09bd6462664b7b2cb331f9191ce07e55aedb
Luis <luis_8.51@hotmail.com>
2022-05-25 15:12:27 -0300

    Herança fixação

---
+++ 0x06/pedido_livraria/Pedido.java
@@ -0,0 +1,24 @@
+import produtos.Livro;
+import produtos.Produto;
+
+public class Pedido {
+
+    protected ItemPedido[] itens;
+    private double percentualDesconto;
+
+    public Pedido(int percentualDesconto, ItemPedido[] itens){
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        double valorTotal=0;
+        for (ItemPedido itemPedido : itens) {
+            valorTotal = valorTotal+(itemPedido.getQuantidade() * itemPedido.getProduto().obterPrecoLiquido());
+
+        }
+        valorTotal = valorTotal - ((percentualDesconto / 100)* valorTotal);
+        return valorTotal;
+    }
+
+}
---
+++ 0x06/produtos/Produto.java
@@ -0,0 +1,55 @@
+public abstract class Produto {
+
+    protected String titulo;
+    protected int ano;
+    protected String pais;
+    protected double precoBruto;
+
+    protected Produto() {
+    }
+
+
+    public abstract double obterPrecoLiquido();
+
+    public Produto(String titulo, int ano, String pais, double precoBruto){
+
+        super();
+        this.titulo = titulo;
+        this.ano = ano;
+        this.pais = pais;
+        this.precoBruto = precoBruto;
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public double getPrecoBruto() {
+        return precoBruto;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setPrecoBruto(double precoBruto) {
+        this.precoBruto = precoBruto;
+    }
+}
