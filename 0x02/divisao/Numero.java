ff2a4470070d447a5165dce975c104afc9c061de
Luis <luis_8.51@hotmail.com>
2022-06-07 11:16:15 -0300

    Exceptions

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,10 @@
+public class Numero {
+    public static void dividir(int a, int b){
+        try{
+            System.out.println(a + " / " + b + " = " + a/b);
+        }catch (ArithmeticException e){
+            System.out.println("Nao e possivel dividir por zero");
+            System.out.println(a + " / " + b + " = " + 0);
+        }
+    }
+}
