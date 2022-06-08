d553a1aa2eeb5868968f4ad412fe55c3c6b4d9a9
Luis <luis_8.51@hotmail.com>
2022-06-08 14:36:29 -0300

    Lambda 2

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,7 @@
+public class FolhaPagamento {
+
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculo) {
+        return calculo.calcular(funcionario.getSalario());
+    }
+}
---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,12 @@
+public class Funcionario {
+
+    private double salario;
+
+    public Funcionario(int salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
