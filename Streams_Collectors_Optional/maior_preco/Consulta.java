850fa4256f443f0fcbf53348e547e011c25aca4c
Luis <luis_8.51@hotmail.com>
2022-06-13 15:58:06 -0300

    Streams 2

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+        List<Produto> pedidoFiltro = pedido.getProdutos().stream()
+                .filter(pedidos -> pedidos.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return pedidoFiltro;
+    }
+
+        public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+
+            Produto pedidoMaior = produtos.stream()
+                .max(Comparator.comparing(pedidos -> pedidos.getPreco())).get();
+
+        return pedidoMaior;
+    }
+
+}
