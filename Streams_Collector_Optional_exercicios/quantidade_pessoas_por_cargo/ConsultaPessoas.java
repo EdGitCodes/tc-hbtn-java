879355c8612b7af47ae4a9f6afc04e61e8c4286d
Luis <luis_8.51@hotmail.com>
2022-06-14 16:05:04 -0300

    Streams 7

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> todasPessoas) {
+
+        //Direto no return para evitar declaração TreeMap<String, Treeset<Pessoa>
+        //Pessoa::getCargo é uma lambda que puxa o cargo
+        //() ->* novo TreeMap apenas para inverter a ordem
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                () -> new TreeMap<> (Comparator.reverseOrder()),
+                Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+}
