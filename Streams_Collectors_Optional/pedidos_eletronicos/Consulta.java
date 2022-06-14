d25722b8c9010ca7ceb5b0fad33b9dbed5df498a
Luis <luis_8.51@hotmail.com>
2022-06-13 21:51:04 -0300

    Streams 4

---
+++ Streams_Collectors_Optional/pedidos_eletronicos/Consulta.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+        List<Produto> pedidoFiltro = pedido.getProdutos().stream()
+                .filter(pedidos -> pedidos.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return pedidoFiltro;
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedido) {
+
+               List<Pedido> pedidoRetorno = pedido.stream().filter(ped -> ped.getProdutos().stream()
+                        .anyMatch(x -> x.getCategoria().equals(CategoriaProduto.ELETRONICO)))
+                        .collect(Collectors.toList());
+
+        return pedidoRetorno;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+
+        Produto pedidoMaior = produtos.stream()
+                .max(Comparator.comparing(pedidos -> pedidos.getPreco())).get();
+
+        return pedidoMaior;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double parametro){
+
+        List<Produto> pedidoMinimo = produtos.stream()
+                .filter(preco -> preco.getPreco() >= parametro).collect(Collectors.toList());
+
+        return pedidoMinimo;
+    }
+}
